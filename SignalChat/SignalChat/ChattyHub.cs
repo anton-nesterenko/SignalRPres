b7329be6c74161d31e89ab24c377e7e63bc49c59
Tomas Jansson <tomas.jansson@gmail.com>
2012-11-08 22:00:15 +0100

    initial commit

---
+++ SignalChat/SignalChat/ChattyHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SignalR.Hubs;
+
+namespace SignalChat
+{
+    [HubName("chatty")]
+    public class ChattyHub : Hub
+    {
+        public void Speak(string name, string message)
+        {
+            Clients.spoke(name, message);
+        }
+    }
+}
---
+++ SignalChat/SignalChat/Scripts/Chatty.js
@@ -0,0 +1,39 @@
+﻿/// <reference path="jquery-1.8.2.js" />
+/// <reference path="jquery-ui-1.9.0.js" />
+/// <reference path="jquery.signalR-0.5.3.js" />
+$(function() {
+    var name;
+    var chatWindows = $(".chat-window");
+    var messageWindow = $("#message");
+    $("#dialog").dialog(
+        {
+            title: "What is your chatty name?",
+            buttons: {
+                "What? My name is!": function() {
+                    name = $("#name").val();
+                    if (name) {
+                        $(this).dialog("close");
+                    }
+                    chatWindows.append('<div class="welcome">Welcome ' + name + '!</div>');
+                }
+            },
+            modal: true,
+            beforeClose: function(event, ui) {
+                if (!name) return false;
+                return true;
+            }
+        });
+    var chatty = $.connection.chatty;
+    $.extend(chatty, {
+        spoke: function(user, message) {
+            chatWindows.append('<div><span class="user-name">' + user + ' said: <span class="words">' + message + '</span></div>');
+        }
+    });
+    $("#speak").click(function() {
+        if (messageWindow.val()) {
+            chatty.speak(name, messageWindow.val());
+            messageWindow.val("");
+        }
+    });
+    $.connection.hub.start();
+});
