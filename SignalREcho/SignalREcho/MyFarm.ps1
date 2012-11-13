348bfec8dc421d2ad7a5249a8d316f4e54febdd1
Tomas Jansson <tomas.jansson@gmail.com>
2012-11-13 23:00:39 +0100

    added farm startup

---
+++ SignalREcho/SignalREcho/MyFarm.ps1
@@ -0,0 +1,18 @@
+#$iisExpressExe = "c:\Program Files (x86)\IIS Express\iisexpress.exe";
+$iisExpressExe = "iisexpress";
+$ports = @(8090, 8091);
+$path = (Resolve-path .);
+
+foreach($port in $ports)
+{
+	Write-Host $path
+	Write-host "Starting site on port: $port"
+	$params = "/port:$port /path:$path"
+	$command = "$iisExpressExe $params"
+#	$scriptBlock = {
+	cmd /c start cmd /k "$command"
+#	}
+#	Start-job $scriptBlock
+}
+
+Get-Job
