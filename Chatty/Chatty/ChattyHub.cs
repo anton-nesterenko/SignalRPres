73a56dc352053e60ebdf0d70290f63c6a95c95fc
Tomas Jansson <tomas.jansson@gmail.com>
2013-01-30 07:32:06 +0100

    updated to NNUG version

---
+++ Chatty/Chatty/ChattyHub.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Chatty
+{
+    [HubName("chatty")]
+    public class ChattyHub : Hub
+    {
+        public void Speak(string message)
+        {
+            string user = Context.ConnectionId;
+            if (message.StartsWith("-p"))
+            {
+                var targetConnectionId = message.Split(' ')[1];
+                message = message.Substring(targetConnectionId.Length + 4);
+                Clients.Client(targetConnectionId).secret(user + ": " + message);
+            }
+            else
+            {
+                Clients.Others.spoke(user + ": " + message);
+            }
+        }
+
+        public override Task OnConnected()
+        {
+            return Clients.Others.connected(Context.ConnectionId);
+        }
+
+    }
+}
---
+++ Echo/Echo/EchoConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Echo
+{
+    public class EchoConnection : PersistentConnection
+    {
+        protected override System.Threading.Tasks.Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            return Connection.Broadcast(connectionId + ": " + data);
+        }
+    }
+}
